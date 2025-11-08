ec62b7c8e6036f25b2d5a74b2d2a048effa20f1a
shubham-choubey-121 <shubhamchoubey393@gmail.com>
2025-11-08 19:48:01 +0530

    Enforce authentication on all task routes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+const { authMiddleware } = require('../middleware/authMiddleware');
 
 const router = express.Router();
+
+// Protect all task routes
+router.use(authMiddleware);
 
 router.get('/', getTasks);
 router.post('/', createTask);
